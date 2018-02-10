b1b294bec9f1e970eee2f58cf1e200724706a553
matbur <mateusz@burniak.pl>
2018-02-10 22:08:35 +0100

    Add 2017/03

---
+++ 2017/03/part1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+def foo(N):
+    d = [0, 0, 0, 0]
+    s = 1
+    for i in range(2, N + 1):
+        n = i // 2
+        d_i = (i - 2) % 4
+        sn = s + n
+        if sn == N:
+            d[d_i] += n
+            break
+        elif sn < N:
+            d[d_i] += n
+            s += n
+        else:
+            d[d_i] += N - s
+            break
+    return d[0] - d[2], d[1] - d[3]
+
+
+def foo2(n):
+    x, y = foo(n)
+    return abs(x) + abs(y)
+
+
+if __name__ == '__main__':
+    print('min')
+    for i in (1, 12, 23, 1024):
+        print(i, foo2(i))
+
+    print('full', foo2(265149))
---
+++ 2017/03/part2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+from part1 import foo
+
+
+def bar(n):
+    m = [np.array([1, 0]),
+         np.array([1, 1]),
+         np.array([0, 1]),
+         np.array([-1, 1]),
+         np.array([-1, 0]),
+         np.array([-1, -1]),
+         np.array([0, -1]),
+         np.array([1, -1])]
+    l = {(0, 0): 1}
+    for i in range(2, n + 2):
+        j = np.array(foo(i))
+        s = 0
+        for k in m:
+            s += l.get(tuple(k + j), 0)
+        l[tuple(j)] = s
+        if s > n:
+            return s
+
+
+if __name__ == '__main__':
+    print('min')
+    for i in range(20):
+        print(i, bar(i))
+
+    print('full', bar(265149))
