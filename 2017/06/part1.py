6449e8052c2f153552ec6e566dd4d31aed5f8cb1
matbur <mateusz@burniak.pl>
2018-02-10 23:47:27 +0100

    Add 2017/06

---
+++ 2017/06/part1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+from utils.files import load_row_int
+
+MIN = load_row_int('minimal.txt')
+FULL = load_row_int('full.txt')
+
+
+def foo(l, verbose=False):
+    l = np.array(l)
+    h = []
+
+    n = len(l)
+
+    if verbose:
+        print(l)
+
+    c = 0
+    while l.tolist() not in h:
+        h.append(l.tolist())
+
+        am = l.argmax()
+        mv = l[am]
+        l[am] = 0
+        for i in range(1, mv + 1):
+            l[(am + i) % n] += 1
+        if verbose:
+            print(l)
+        c += 1
+    return c
+
+
+if __name__ == '__main__':
+    print('min', foo(MIN, True))
+    print('full', foo(FULL))
---
+++ 2017/06/part2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+from part1 import MIN, FULL
+
+
+def bar(l, verbose=False):
+    l = np.array(l)
+    h = {}
+
+    n = len(l)
+
+    if verbose:
+        print(l)
+
+    c = 0
+    while tuple(l) not in h:
+        h[tuple(l)] = c
+
+        am = l.argmax()
+        mv = l[am]
+        l[am] = 0
+        for i in range(1, mv + 1):
+            l[(am + i) % n] += 1
+        if verbose:
+            print(l)
+        c += 1
+    return c - h[tuple(l)]
+
+
+if __name__ == '__main__':
+    print('min', bar(MIN, True))
+    print('full', bar(FULL))
