2f54f0c74fc484270441af6985109f027a75f77f
matbur <mateusz@burniak.pl>
2018-03-01 17:41:30 +0100

    Add 2017/07 part1

---
+++ 2017/07/part1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from utils.files import load_matrix
+
+MIN = load_matrix('minimal.txt')
+FULL = load_matrix('full.txt')
+
+
+def foo(l, verbose=False):
+    d = {}
+    for k, *v in l:
+        d[k] = set(i.strip(',') for i in v[2:])
+
+    s = l[-1][0]
+    while True:
+        for k, v in d.items():
+            if s in v:
+                s = k
+                break
+        else:
+            return s
+
+
+if __name__ == '__main__':
+    print('min', foo(MIN, True))
+    print('full', foo(FULL))
